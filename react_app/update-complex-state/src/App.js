60524c9c24d7e5fdfc2c4bfff3162e90f3402b02
Stan <potomak123@googlemail.com>
2018-08-20 09:44:36 +0200

    State excercise solution

--- react_app/update-complex-state/src/App.js
+++ react_app/update-complex-state/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     this.state = {
       instructors: [
         {
-          name: 'Time',
+          name: 'Tim',
           hobbies: ['sailing', 'react']
         },
         {
@@ -24,7 +24,48 @@ class App extends Component {
         }
       ]
     }
+    setTimeout(() => { // dzięki temu, że jest funkcja strzałkowa to słowo this odnosi się do App
+      //find random instructor
+      const randInst = Math.floor(Math.random() * this.state.instructors.length);
+      const hobbyIndex = Math.floor(Math.random() * this.state.instructors[randInst].hobbies.length);
+
+
+      // *******************************
+      // const instructors = this.state.instructors.slice();
+      // instructors[randInst] = Object.assign({}, instructors[randInst]);
+      // instructors[randInst].hobbies = instructors[randInst].hobbies.slice();
+      // instructors[randInst].hobbies.splice(hobbyIndex, 1);
+      // this.setState({instructors});
+      // *******************************
+
+      // lepsza metoda - skrócona ale trudniejsza
+      // const instructors = this.state.instructors.map((inst, i) =>( // map zwraca nową tablicę, nie modyfikuje oryginalnej tablicy
+      //   i === randInst ? { // jesli i równa się randInst to chcemy coś zmienić
+      //     ...inst, // chcemy zachować wszystkie keys w tym obiekcie ( w tym są tylko dwa )
+      //     hobbies: [...inst.hobbies.slice(0, hobbyIndex).concat(inst.hobbies.slice(hobbyIndex+1, inst.hobbies.length))]
+      //   } : inst // w pozostałych nic nie zmieniamy i zwracamy oryginalny stan
+      // )); // jeśli wstawiamy nawiasy okrągle zamiast klamrowych to pomijamy słowo return
+
+      // lepsza - dłuższa
+      const instructors = this.state.instructors.map((inst, i) => {
+        if(i === randInst) {
+          const hobbies = [inst.hobbies]; // to samo co inst.hobbies.slice();
+          hobbies.splice(hobbyIndex, 1);
+          return {
+            ...inst,
+            hobbies
+          }
+        }
+
+        return inst;
+      })
+      this.setState({instructors});
+
+    }, 1000);
   }
+
+
+
   render() {
     const instructors = this.state.instructors.map((instructor, index) => (
       <li key={index}>
